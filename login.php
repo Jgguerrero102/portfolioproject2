bd0a59c1da4a646a420250d66a25dc55b6bbb097
Jgguerrero102 <97700013+Jgguerrero102@users.noreply.github.com>
2022-04-30 23:45:09 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,22 @@
-<!DOCTYPE html>
+<?php
+    $_SESSION;
 
+?>
+
+
+<!DOCTYPE html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+<div id="box">
+<div>Login</div>
+    <input type="text" name="user_name">
+    <br>
+    <input type="password" name="password">
+    <input type="submit" value="Login">
+
+    <a href="Logim.php">Login</a>
+</div>
+</body>
 </html>
